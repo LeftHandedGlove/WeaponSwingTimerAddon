d9721db6f524db37b0e47115ba70883dbf8866f1
David Delamater <david.delamater@baesystems.com>
2019-05-21 18:02:54 -0500

    Fine tuning swing text

--- WeaponSwingTimer_MainFrame.lua
+++ WeaponSwingTimer_MainFrame.lua
@@ -49,7 +49,7 @@ LHGWSTMain.UpdateSwingFrames = function()
 	-- Update the players swing text
 	player_offset = (main_frame:GetWidth() - 2) - player_width
 	player_swing_frame.swing_text:SetText(tostring(SimpleRound(play_swing_time, 0.1)))
-	player_swing_frame.swing_text:SetPoint("BOTTOMRIGHT", player_offset + 25, 3)
+	player_swing_frame.swing_text:SetPoint("BOTTOMRIGHT", player_offset + 25, 1)
     -- Update the target swing frame
     local target_swing_frame = LHGWSTMain.main_frame.target_swing_frame
 	local target_percent = 1 - (tar_swing_time / tar_weap_speed)
@@ -58,7 +58,7 @@ LHGWSTMain.UpdateSwingFrames = function()
 	-- Update the targets swing text
 	target_offset = (main_frame:GetWidth() - 2) - target_width
 	target_swing_frame.swing_text:SetText(tostring(SimpleRound(tar_swing_time, 0.1)))
-	target_swing_frame.swing_text:SetPoint("TOPRIGHT", target_offset + 25, -3)
+	target_swing_frame.swing_text:SetPoint("TOPRIGHT", target_offset + 25, -1)
 	-- Update the CRP Ping delay
 	main_frame.target_swing_frame.crp_ping_frame:SetHeight(main_frame.target_swing_frame:GetHeight() / 2)
 	local down, up, lagHome, lagWorld = GetNetStats()
@@ -121,7 +121,7 @@ LHGWSTMain.CreateLHGWSTMainFrame = function()
 	main_frame.player_swing_frame.swing_text = main_frame.player_swing_frame:CreateFontString(nil, "ARTWORK")
 	main_frame.player_swing_frame.swing_text:SetFont("Fonts/FRIZQT__.ttf", 11)
     main_frame.player_swing_frame.swing_text:SetJustifyV("CENTER")
-    main_frame.player_swing_frame.swing_text:SetJustifyH("CENTER")
+    main_frame.player_swing_frame.swing_text:SetJustifyH("LEFT")
     -- Setup the target's swing image appearance
     main_frame.target_swing_frame = CreateFrame("Frame", "WSTTargetSwingFrame", main_frame)
     local target_texture = main_frame.target_swing_frame:CreateTexture(nil,"ARTWORK")
@@ -132,7 +132,7 @@ LHGWSTMain.CreateLHGWSTMainFrame = function()
 	main_frame.target_swing_frame.swing_text = main_frame.target_swing_frame:CreateFontString(nil, "ARTWORK")
     main_frame.target_swing_frame.swing_text:SetFont("Fonts/FRIZQT__.ttf", 11)
     main_frame.target_swing_frame.swing_text:SetJustifyV("CENTER")
-    main_frame.target_swing_frame.swing_text:SetJustifyH("CENTER")
+    main_frame.target_swing_frame.swing_text:SetJustifyH("LEFT")
 	-- Setup the Crit Reactive Procs Ping Delay Frame
 	main_frame.target_swing_frame.crp_ping_frame = CreateFrame("Frame", "WSTCRPPingFrame", main_frame.target_swing_frame)
 	local crp_ping_texture = main_frame.target_swing_frame.crp_ping_frame:CreateTexture(nil,"ARTWORK")
